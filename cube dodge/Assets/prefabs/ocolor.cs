d99274069fbe4475fb9a177523b23bd8c3e06f1a
John Nesbit <49009243+JohnNesbit@users.noreply.github.com>
2020-10-05 19:32:04 -0400

    Add files via upload

---
+++ cube dodge/Assets/prefabs/ocolor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ocolor : MonoBehaviour
+{
+    public GameObject o;
+    public int level;
+    public Material[] mymats;
+    void Update()
+    {
+	level = PlayerPrefs.GetInt("level");
+        while(level > 5){
+		level -= 5;
+		}
+	Debug.Log(level);
+	GetComponent<Renderer>().material = mymats[level-1];
+    }
+}
---
+++ cube dodge/Assets/prefabs/pcolor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pcolor : MonoBehaviour
+{
+    public GameObject o;
+    public int level;
+    public Material[] mymats;
+    void Update()
+    {
+	level = PlayerPrefs.GetInt("level");
+        while(level > 5){
+		level -= 5;
+		}
+	GetComponent<Renderer>().material = mymats[level-1];
+    }
+}
